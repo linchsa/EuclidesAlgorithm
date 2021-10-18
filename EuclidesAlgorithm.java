5afee241ac9d9e7f1f4c9b30ef30dc7659405b7a
CelerX <54667438+linchsa@users.noreply.github.com>
2021-10-18 18:09:58 +0200

    Add files via upload

---
+++ EuclidesAlgorithm.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class EuclidesAlgorithm {
+    static int EuclidesAlgorithmfunction(int x,int y){
+        System.out.println("mcd("+x+","+y+")");
+        int r=x%y;
+        if(r==0){
+            System.out.println("mcd("+y+","+r+")");
+            return y;
+        }
+        else{
+            return EuclidesAlgorithmfunction(y,r);
+        } 
+    }
+    public static void main(String[] args) {
+    int prueba2=EuclidesAlgorithmfunction(194, 70);
+    System.out.println(prueba2);
+    }
+}
